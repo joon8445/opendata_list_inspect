54798ff1f5e398bba26dc2aa06e6c0b7a2973619
s-jun <joon8445@gmail.com>
2021-11-09 09:23:33 +0900

    zip파일 내부 확장자 확인 추가

--- extend_check.py
+++ extend_check.py
@@ -1,5 +1,5 @@
 import wget
-
+from zipfile import ZipFile
 def extend_check(bsObject, table_df):
     # 첨부파일 확장자
     try:
@@ -26,7 +26,16 @@ def extend_check(bsObject, table_df):
             if file_extension.lower() == extend:
                 result_5 = '적합'
             elif extend == 'zip':
-                result_5 = '직접확인(zip)'
+                with ZipFile(ans, 'r') as zipObj:
+                    listOfFileNames = zipObj.namelist()
+                for fileName in listOfFileNames:
+                    fileList = fileName.split('.')
+                    extend = fileList[-1]
+                    if file_extension.lower() == extend:
+                        result_5 = '적합'
+                        break
+                    else:
+                        result_5 = '오류 : zip파일 내부에 '+file_extension+'파일이 존재하지 않습니다.'
             else:
                 result_5 = '오류 : 첨부파일의 확장자가 잘못 등록되었음\n' + file_extension + ' -> ' + extend + '로 수정 요망'
 
